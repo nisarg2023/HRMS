c1abacd0295e1ec6bcae90d21b90f928123a32bf
Divyang <divyang.tarbumdiya.23@gmail.com>
2023-03-16 14:18:07 +0530

    23dev065 database name

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,6 +1,6 @@
 async function conn () {
     const mysql = require('mysql2/promise');
-    const conn = await mysql.createConnection({ host:'localhost', user: 'root',  password: 'root',database: 'login' });
+    const conn = await mysql.createConnection({ host:'localhost', user: 'root',  password: 'root',database: 'hrms' });
     return conn
 }
   

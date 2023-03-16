a81e49297d43e13cfba090bd6def06389d40d674
chirag-talpada <chiragvaghela492@gmail.com>
2023-03-16 18:39:40 +0530

    23DEV052: Designed Wizard Form

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router=express.Router();
 
 const loginController=require('../controllers/login.controller');
+const employeeController=require('../controllers/employee-data.controller');
 
 router.get('/',loginController);
+
+router.get('/employee-data',employeeController);
 
 
 module.exports=router;

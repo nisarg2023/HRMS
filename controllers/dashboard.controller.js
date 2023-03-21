ba370208901497298fb042b0dcea3281ae43ca0e
Divyang <divyang.tarbumdiya.23@gmail.com>
2023-03-21 18:39:32 +0530

    23DEV065 to add comment in database and show in frontend

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,3 +1,7 @@
+const conn = require("../config/dbConnect");
+const util = require('util');
+const query = util.promisify(conn.query).bind(conn);
+
 const getDashboard = (req,res)=>{
     res.render('dashboard')
     //res.send("hello");
@@ -11,5 +15,34 @@ const getHotlines = (req,res)=>{
 const getAttendance = (req,res)=>{
     res.render('attendance');
 }
+const getComment = async (req,res)=>{
+    var comment = req.query.comment;
+    // console.log("hwifhwhfifgiuwgfijkgwsfuigtwufgswjfswjfgweikfgsdu");
+    // res.send("Your Comment ")
+    console.log(comment);
+    var commentQuery = `insert into employee_comment (fk_emp_id,comment) values (1,'${comment}');`
+    var commentData=await query(commentQuery);
+    res.json();
+    // console.log(commentData);
+}
+const getCommentId = async (req,res)=>{
+    var commentId= req.query.commentId;
+    if(commentId){
 
-module.exports = {getDashboard,getHotlines,getAttendance}
+        console.log(commentId);
+        var idQuery=`update employee_comment  set comment_status="1" where emp_comment_id="${commentId}";`
+        var idData= await query(idQuery);
+    }
+    else{
+        res.send("Could not read comment")
+    }
+}
+const getCommentData = async (req,res)=>{
+    var allCommentQuery=`select * from employee_comment;`
+    var allCommentData= await query(allCommentQuery);
+    res.render('allComment',{c:allCommentData});
+    // console.log(allCommentData);
+}
+
+
+module.exports = {getDashboard,getHotlines,getAttendance,getComment,getCommentData,getCommentId}
--- routes/deshbord.route.js
+++ routes/deshbord.route.js
@@ -7,6 +7,9 @@ const leaveApprove = require('../controllers/leave-approve.controller')
 router.get('/',deshbord.getDashboard);
 router.get('/get-hotline',deshbord.getHotlines);
 router.get('/get-attendance',deshbord.getAttendance);
+router.get('/get-comment',deshbord.getComment);
+router.get('/allComment',deshbord.getCommentData);
+router.get('/comment-id',deshbord.getCommentId);
 
 router.get('/leave',leaveApplication.getLeaveapplication)
 router.post('/leave',leaveApplication.postLeaveapplication)

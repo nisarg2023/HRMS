0c0749181e43f237178ee2b7c809f173c9593aba
Sanisinh2710 <esparkbiz.sanisinh@gmail.com>
2023-03-24 18:38:35 +0530

    bug fixex

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -10,7 +10,7 @@ const getUserBasicinfo = async(id = "") => {
         return data;
     } else {
 
-        const data = await query(`SELECT basic_info_id,first_name FROM hrms.basic_info where fk_emp_id = ${id};`)
+        const data = await query(`SELECT * FROM hrms.basic_info where fk_emp_id = ${id};`)
         return data;
     }
 
@@ -113,8 +113,9 @@ const getDataProfile= async(req,res)=>{
     const email = req.session.email;
     const profilePhoto =  await getUserProfilePhoto(["profile_photo"],req.session.emp_id);
     const e_id = req.session.emp_id;
-    const document_query = await query(`select * from document where fk_emp_id = "${e_id}"`);
 
+    console.log(userInfo[0]);
+    const document_query = await query(`select * from document where fk_emp_id = "${e_id}"`);
     res.render('viewProfile',{"first_name": userInfo[0].first_name,dataset: userInfo[0],allUsers,profilePhotos,"profilePhoto":profilePhoto[0].profile_photo,email,document:document_query});
     
 

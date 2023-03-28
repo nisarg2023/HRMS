17fc8f97da8826c529310b30c739a197248655cc
nisarg2023 <nisarg.rakholiya.23.gmail.com>
2023-03-28 15:44:20 +0530

    23DEV006 23DEV069 remove localhost in all url

--- public/script/checkin.js
+++ public/script/checkin.js
@@ -226,7 +226,7 @@ let commentContainer = document.getElementById('comment-container')
 
 async function addComment() {
     var comment = document.getElementById('comment').value;
-    fetch(`http://localhost:8000/dashbord/get-comment?comment=${comment}`, {
+    fetch(`/dashbord/get-comment?comment=${comment}`, {
         method: "GET",
         headers: {
             "Content-Type": "application/json"
@@ -248,7 +248,7 @@ async function addComment() {
 
 const updateCommentCard = async() => {
     commentContainer.innerHTML = ''
-    await fetch(`http://localhost:8000/dashbord/updateCommentCard`)
+    await fetch(`/dashbord/updateCommentCard`)
         .then(res => res.json())
         .then(data => {
             data.forEach((singleComment) => {

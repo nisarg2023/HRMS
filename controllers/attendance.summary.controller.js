1796f974f8af2cc45090bdb153b0d99429aff23a
khatrisagar <khatrisagar404@gmail.com>
2023-04-10 14:56:50 +0530

    23DEV069 record your attendance card for dashboard

--- controllers/attendance.summary.controller.js
+++ controllers/attendance.summary.controller.js
@@ -34,63 +34,18 @@ const getUserProfilePhoto = async(fields = "*", id = "") => {
 
 
 
-let dailyBreakTime = 0;
-let monthlyWorkHours = 0;
-let monthlyBreakArr = [];
-let dailyWorkHoursArr = [];
 
-
-
+const attendance =  require('./dashboardprofile.controller')
 
 const attendancy_summary = async(req, res) => {
 
-
-    dailyBreakTime = 0;
-    monthlyWorkHours = 0;
-    monthlyBreakArr = [];
-    dailyWorkHoursArr = [];
+    let attendanceData = await attendance.empolyeeAttendanceData(req)
 
 
-    
-    let querychecktime = `SELECT * FROM check_system where basic_info_id = ${req.session.emp_id}`;
-    
-    let data1 = await query(querychecktime);
-    console.log("check query",data1)
-
-    
- 
-
-
-    
-    for (let i = 0; i < data1.length; i++) {
-        dailyWorkHoursArr.push(data1[i].total_office_time);
-        monthlyWorkHours += Number(data1[i].total_office_time);
-        // in minutes
-        
-
-
-    }
-
-    for (let i = 0; i < data1.length; i++) {
-        console.log("checkdate",data1[i].check_date)
-        let querybraketime = `select total_brake_time from brake_system where brake_date ="${moment(data1[i].check_date).format("YYYY-MM-DD")}" and basic_info_id = "${data1[i].basic_info_id}";`
-
-        let data2 = await query(querybraketime);
-        console.log("break data", data2,data1[i].basic_info_id)
-        for (let i = 0; i < data2.length; i++) {
-            dailyBreakTime += Number(data2[i].total_brake_time);
-        }
-
-        monthlyBreakArr.push(dailyBreakTime);
-
-        dailyBreakTime = 0;
-
-
-    }
     const userInfo = await getUserBasicinfo(req.session.emp_id);
     const profilePhoto = await getUserProfilePhoto(["profile_photo"], req.session.emp_id);
 
-    res.render("attendance", { data1, dailyBreakTime, monthlyBreakArr, dailyWorkHoursArr,monthlyWorkHours, "first_name": userInfo[0].first_name, "profilePhoto": profilePhoto[0].profile_photo, "tz":`${parseInt(req.cookies.tz)}` })
+    res.render("attendance", {attendanceData, "first_name": userInfo[0].first_name, "profilePhoto": profilePhoto[0].profile_photo, "tz":`${parseInt(req.cookies.tz)}` })
 }
 
 module.exports = { attendancy_summary }
